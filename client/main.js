8734b3d2d7d2090925dee65bc7cfb0eec2da5736
Florian Mettetal <florian.mettetal@flex.com>
2017-07-28 08:50:00 -0700

    Examples improved with a refactor Fixed data passing w removal of import of 'meteor/templating' [Reference](https://stackoverflow.com/questions/45359959/meteor-dynamic-template-data-context/45377536#45377536)

--- client/main.js
+++ client/main.js
@@ -1,4 +1,3 @@
-import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
@@ -9,16 +8,26 @@ Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
-
 Template.hello.helpers({
-  counter() {
+  count() {
     return Template.instance().counter.get();
-  },
-});
-
+  }
+})
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+Template.info.helpers({
+  links() {
+    const Link = (target,name) => ({target,name})
+    return [
+      Link("https://www.meteor.com/try", "Do the Tutorial"),
+      Link("http://guide.meteor.com", "Follow the Guide"),
+      Link("https://docs.meteor.com", "Read the Docs"),
+      Link("https://forums.meteor.com", "Discussions")
+    ]
   },
 });
